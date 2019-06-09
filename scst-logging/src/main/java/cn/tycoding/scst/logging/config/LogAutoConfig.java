808a3f17724d86092a3c38ca428daa106867d711
tycoding <2557988481@qq.com>
2019-06-09 12:31:48 +0800

    :bookmark: init repo

--- scst-logging/src/main/java/cn/tycoding/scst/logging/config/LogAutoConfig.java
+++
@@ -1,8 +0,0 @@
-package cn.tycoding.scst.logging.config;
-
-/**
- * @author tycoding
- * @date 2019-06-08
- */
-public class LogAutoConfig {
-}
---
+++ scst-logging/src/main/java/cn/tycoding/scst/logging/utils/LogUtil.java
@@ -0,0 +1,8 @@
+package cn.tycoding.scst.logging.utils;
+
+/**
+ * @author tycoding
+ * @date 2019-06-09
+ */
+public class LogUtil {
+}
